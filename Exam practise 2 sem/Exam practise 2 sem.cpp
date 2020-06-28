072e2e25f62cbb452ae15a6724b6cde327513e4a
zeils <maksimmovchan25@mail.ru>
2020-06-29 00:58:37 +0300

    students_list

--- Exam practise 2 sem/Exam practise 2 sem.cpp
+++ Exam practise 2 sem/Exam practise 2 sem.cpp
@@ -15,11 +15,11 @@ int school_popularity = 0;
 class Students{
 	// Имя студента
 	public: 
-         string name;
-
-		 float score;
+    string name;
+    float score;
+	int ID; //id студента
 	// Фамилия
-	std::string last_name;
+	string last_name;
 	// Пять промежуточных оценок студента
 	int marks[5]{ 5,5,5,5,4 };
 	// Итоговая оценка за семестр
@@ -32,10 +32,6 @@ class Students{
 	}
 	
 
-
-
-
-
 	template <class T> float Get_score (T *B , T cout = 5)
 	{
 		 //std::cout << "sum(B,cout)" << sum(B, cout) << std::endl;
@@ -47,13 +43,10 @@ class Students{
 
 
 
-	int ID; //id студента
-
-
-	Students(string name = "no_name", string lastname = "no_lastname" ) // без описания равно 0 - конструктор
+	Students(string name1 = "no_name", string lastname1 = "no_lastname" ) //  - конструктор
 	{
-		name = "no_name";
-		last_name = "no_lastname";
+		name = name1;
+		last_name = lastname1;
 		school_popularity++;
         score = Get_score(marks); 
 		ID = school_popularity;
@@ -62,10 +55,10 @@ class Students{
 
 	~Students() // деструктор
 	{
-		cout << "kill student";
+		cout << "kill student" << endl;
 		school_popularity--;
 	}
-    private: float average_ball;
+
 			
     
 };
@@ -78,17 +71,29 @@ int main()
 {    
 	
 	// студенты
-	Students *new_people = new Students("имя" , "фамилия" ); // создадим пустого студента
+	Students new_people = Students("name" , "surname" ); // создадим пустого студента
 	std::cout << school_popularity << std::endl;
-	std::cout << new_people->score << std::endl;
-	list <Students> spisok;
-	spisok.insert(spisok.end(), new_people);
-    Big_gun gun1;
+	std::cout << new_people.score << std::endl;
+	std::cout << new_people.name << std::endl;
+
+
+	Students newest = Students("I", "B");
+
+
+
+	list <Students> School;
+	School.insert(School.end(), newest);
+
+
+
+    Big_gun gun1; 	//оружие (virtual_gun)
 	Player player1;
 	player1.Shoot(&gun1);
-	delete new_people; // и принесем в жертву 
 
-	//оружие (virtual_gun)
+
+	//delete &new_people; // и принесем в жертву 
+
+
 	
 	
 
